60f1f2b433ac09e088908369b9ffe2640f4fd7e3
Eric Shin <92757570+eshin-at-speer@users.noreply.github.com>
2022-01-20 22:22:53 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>this is Quizzler</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
